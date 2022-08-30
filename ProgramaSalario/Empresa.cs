2914407499af41fd53a6e38599e26bee28180d7b
Erik Lisboa <bonitoerik@gmail.com>
2022-08-30 01:12:49 -0300

    Add project files.

---
+++ ProgramaSalario/Empresa.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace ProgramaSalario
+{
+    class Empresa
+    {
+        public string Nome;
+        public string Cargo;
+        public int Funcionarios;
+        public double Salarios;
+
+        public double ReceitaTotal()
+        {
+            return Funcionarios * Salarios;
+        }
+        public void ContratarFuncionarios(int funcionarios)
+        {
+            Funcionarios += funcionarios;
+        }
+        public void DemitirFuncionarios(int funcionarios)
+        {
+            Funcionarios -= funcionarios;
+        }
+        
+        public override string ToString()// aqui vai estar o que vai conter em P
+        {
+            return $" A empresa {Nome} deseja acessa o cargo: {Cargo}\n " +
+                $" Quantidade funcionarios:{Funcionarios}\n " +
+                $"salario: {Salarios}\n " +
+                $"Receita total da empresa :" + ReceitaTotal().ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+    }
+
+ 
+}
---
+++ ProgramaSalario/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Globalization;
+
+namespace ProgramaSalario
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+        Empresa e = new Empresa();//Instaciamento 
+
+            Console.WriteLine("Qual nome da sua empresa:");
+            e.Nome = Console.ReadLine();
+            Console.WriteLine("Qual cargo voce deseja acessa?");//Verificar uma logica com o ESCOLHA/SWITCH
+            e.Cargo = Console.ReadLine();
+            Console.WriteLine($"Quantos funcionarios exerce o cargo de {e.Cargo} sua empresa tem?");
+            e.Funcionarios = int.Parse(Console.ReadLine());
+            Console.WriteLine("Qual salario a empresa paga pra quem exerce este cargo");
+            e.Salarios = double.Parse(Console.ReadLine());
+
+            //dados
+            Console.WriteLine("DADOS DA EMPRESA " + e);
+            Console.WriteLine();
+            Console.WriteLine("Quantos funcionarios voce deseja contratar?");
+            int func = int.Parse(Console.ReadLine());
+            e.ContratarFuncionarios(func);
+            Console.WriteLine();
+            Console.WriteLine("DADOS ATUALIZADOS " + e);
+            Console.WriteLine();
+            Console.WriteLine("Quanto funcionarios voce deseja demitir?");
+            int dem = int.Parse(Console.ReadLine());
+            e.DemitirFuncionarios(dem);
+            Console.WriteLine("DADOS ATUALIZADOS " + e);
+
+
+
+
+        }
+    }
+}
